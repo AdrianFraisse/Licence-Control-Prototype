faaec786166d207912a8edb0fefc7ad2da4ad3cb
yolagach <lagache.yoann@gmail.com>
2014-06-06 14:25:54 +0200

    Import du projet DAO + utilisation de ce projet dans le server

--- Licence Control Prototype - Server/src/fr/emn/prototype/licencecontrol/server/LicenceControl.java
+++ Licence Control Prototype - Server/src/fr/emn/prototype/licencecontrol/server/LicenceControl.java
@@ -1,13 +1,16 @@
 package fr.emn.prototype.licencecontrol.server;
 
+import java.sql.SQLException;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-import fr.emn.prototype.licencecontrol.data.DAO;
-import fr.emn.prototype.licencecontrol.data.DAOStub;
+import daoimpl.DAOLicence;
+import daointerface.DAO;
+
 
 @Path("/licence")
 public class LicenceControl {
@@ -21,8 +24,13 @@ public class LicenceControl {
 	}
 	
 	private boolean checkData(String licence, String checksum) {
-		DAO dao = new DAOStub();
-		return dao.checkLicence(licence) && dao.getChecksum(licence).equals(checksum);
+		DAO dao = DAOLicence.instance();
+		try {
+			return dao.checkLicence(licence) && dao.getChecksum(licence).equals(checksum);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 	
 }
