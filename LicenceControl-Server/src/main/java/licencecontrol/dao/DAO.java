2065f28bfd7de9ea3265329b76bc99767d0807bc
yolagach <lagache.yoann@gmail.com>
2014-06-11 19:38:57 +0200

    Modified DAO interface

--- LicenceControl-Server/src/main/java/licencecontrol/dao/DAO.java
+++ LicenceControl-Server/src/main/java/licencecontrol/dao/DAO.java
@@ -7,15 +7,7 @@ public interface DAO {
 	public boolean validateLicence(String licence) throws DAOException;
 	public String getChecksum(String licence) throws DAOException;
 	public int getNbMaxUsers(String licence) throws DAOException;
-	/**
-	 * Insère une clé temporaire dans la table Session
-	 * @param licence La licence sur laquelle porte la clé temporaire
-	 * @param key La clé temporaire
-	 * @param timestamp La date d'expiration de la clé temporaire
-	 * @return true si insertion effectuée, sinon false
-	 */
-	public boolean insertTemporaryKey(String licence, String key, Timestamp timestamp) throws DAOException;
-	
-	int getNbActiveSessions(String licence);
+	public boolean insertTemporaryKey(String licence, String tempKey, Timestamp timestamp) throws DAOException;
+	public int getNbActiveSessions(String licence);
 	
 }
